b3eb78da8d063a792a874c6476a227f0391f0dae
lubeina <52536093+lubeina@users.noreply.github.com>
2020-03-10 20:41:12 +0300

    Cart Created

---
+++ components/CakeCart/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { observer } from "mobx-react";
+
+// NativeBase Components
+import { Text, List, Button } from "native-base";
+
+// Component
+import CartItem from "./CartItem";
+
+// Stores
+import cartStore from "../../stores/cartStore";
+
+const CakeCart = () => {
+  const cartItems = cartStore.items.map(item => (
+    <CartItem item={item} key={`${item.drink} ${item.option}`} />
+  ));
+
+  return (
+    <List>
+      {cartItems}
+      <Button full danger onPress={cartStore.checkoutCart}>
+        <Text>Checkout</Text>
+      </Button>
+    </List>
+  );
+};
+
+CakeCart.navigationOptions = {
+  title: "Cart"
+};
+
+export default observer(CakeCart);
--- navigation/ProfileNav.js
+++ navigation/ProfileNav.js
@@ -3,11 +3,13 @@ import { createStackNavigator } from "react-navigation-stack";
 // Components
 import ProfileScreen from "../components/Profile";
 import LoginScreen from "../components/Login";
+import CartScreen from "../components/CakeCart";
 
 const ProfileStack = createStackNavigator(
   {
     Profile: ProfileScreen,
-    Login: LoginScreen
+    Login: LoginScreen,
+    Cart: CartScreen
   },
   {
     initialRouteName: "Profile",
